e17c7c8bd1ef192aa79625264f41d0f83edccfc5
Henry <henry@henrywu.co.uk>
2021-04-12 17:08:15 +0100

    init commit

---
+++ Assets/Frizzle/ScheduleController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.Networking;
+using SimpleJSON;
+using System;
+using TMPro;
+
+
+public class ScheduleController : MonoBehaviour {
+  private const string getURL = "https://api-6311.herokuapp.com/lectures/pending";
+  private TextMeshProUGUI t;
+
+  void Start() {
+    t = GetComponent<TextMeshProUGUI>();
+    t.text = "";
+    GetSchedule();
+  }
+
+  public void GetSchedule() {
+    StartCoroutine(GetRequest());
+  }
+
+  IEnumerator GetRequest() {
+    UnityWebRequest www = UnityWebRequest.Get(getURL);
+
+    yield return www.SendWebRequest();
+
+    if (www.isNetworkError || www.isHttpError) {
+      Debug.LogError(www.error);
+    } else {
+      var res = JSON.Parse(www.downloadHandler.text);
+
+      for (int i = 0; i < res.Count; i++) {
+        string name = res[i]["name"].Value;
+        double start = res[i]["start"].AsDouble;
+        DateTime date = DateTimeOffset.FromUnixTimeMilliseconds((long)start).LocalDateTime;
+        string formattedDate = date.ToString("hh:mm tt dddd d MMMM");
+
+        string entry = name + " \u000a" + formattedDate + " \u000a\u000a";
+        t.text += entry;
+      }
+    }
+  }
+}
---
+++ Assets/Frizzle/VideoController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Networking;
+using SimpleJSON;
+using System;
+using TMPro;
+
+public class VideoController : MonoBehaviour {
+  private const string getURL = "https://api-6311.herokuapp.com/lectures/active";
+  private VideoPlayer video;
+  private ScheduleController schedule;
+  private GameObject screen;
+  private bool _needNewUrl = false;
+  private bool _noUpcomingVideo = false;
+  private float _timer = 0f;
+  private float _ratio;
+  private long _manualTimestamp;
+
+  private string _url = "";
+  private long _startsAt;
+  private long _expiresAt;
+
+  private bool _end = true;
+
+  [SerializeField] private TextMeshProUGUI t;
+
+  void Start() {
+    video = GetComponent<VideoPlayer>();
+    schedule = GetComponent<ScheduleController>();
+    video.loopPointReached += EndReached;
+    StartCoroutine(GetActiveVideo());
+  }
+
+  void Update() {
+    DateTime now = DateTime.UtcNow;
+    long timeNow = new DateTimeOffset(now).ToUnixTimeMilliseconds();
+
+    if (_end && _url != "") {
+      if (timeNow > _startsAt) {
+        video.url = _url;
+        video.Play();
+        _end = false;
+        //gameObject.transform.localScale = new Vector3(_ratio * 4.5f, 4.5f, 0.1f);
+      }
+    }
+
+    // get a new url from the server 20s before the start time 
+    if (_needNewUrl && timeNow > _startsAt - 20000) {
+      StartCoroutine(GetActiveVideo());
+      _needNewUrl = false;
+    }
+
+    // check every 10m if there's no upcoming video
+    if (_noUpcomingVideo) {
+      _timer += Time.deltaTime;
+      Debug.Log(_timer);
+      if (_timer > 10 * 60) {
+        StartCoroutine(GetActiveVideo());
+        _noUpcomingVideo = false;
+      }
+    }
+
+    // prevent video from pausing if the device going in to standby
+    _manualTimestamp = timeNow - _startsAt;
+    if (video.isPlaying && Math.Abs(video.time - _manualTimestamp / 1000) > 10) {
+      video.time = (int)_manualTimestamp / 1000;
+    }
+  }
+
+  IEnumerator GetActiveVideo() {
+    UnityWebRequest www = UnityWebRequest.Get(getURL);
+
+    yield return www.SendWebRequest();
+
+    if (www.isNetworkError || www.isHttpError) {
+      Debug.LogError(www.error);
+    } else {
+      var d = JSON.Parse(www.downloadHandler.text);
+
+      if (d["message"].Value == "no upcoming events") {
+        _noUpcomingVideo = true;
+      } else {
+        _url = d["url"].Value;
+        _startsAt = (long)d["start"].AsDouble;
+        _expiresAt = (long)d["expires"].AsDouble;
+        _ratio = d["ratio"].AsFloat;
+
+        // if the url expires before the video is scheduled to start
+        if (_expiresAt < _startsAt) {
+          _needNewUrl = true;
+        }
+      }
+    }
+  }
+
+  void EndReached(VideoPlayer vp) {
+    _end = true;
+    _url = "";
+    _startsAt = 0;
+    _expiresAt = 0;
+    video.Stop();
+
+    StartCoroutine(GetActiveVideo());
+    schedule.GetSchedule();
+  }
+}

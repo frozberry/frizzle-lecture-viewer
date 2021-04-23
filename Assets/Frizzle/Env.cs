38a49a17544e14d7bd718e55bd82bb6b53997656
Henry <henry@henrywu.co.uk>
2021-04-23 21:12:20 +0100

    create switch to toggle vr emulator and speed

---
+++ Assets/Frizzle/Env.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+
+public class Env : MonoBehaviour {
+  public bool production;
+  [SerializeField] private VREmulator emulator;
+  [SerializeField] private SmoothLocomotion locomotion;
+
+  void Start() {
+    if (production) {
+      emulator.EmulatorEnabled = false;
+      locomotion.MovementSpeed = 0;
+    } else {
+      emulator.EmulatorEnabled = true;
+      locomotion.MovementSpeed = 5;
+    }
+  }
+}

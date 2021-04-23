20858aeea1d1e9bd9004ee8bacfec7c6f212a395
Henry <henry@henrywu.co.uk>
2021-04-23 08:14:48 +0100

    create progress bar

---
+++ Assets/CircularProgressBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CircularProgressBar : MonoBehaviour {
+  private Image bar;
+  float currentValue;
+  public float speed;
+  void Start() {
+    bar = GetComponent<Image>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (currentValue < 100) {
+      currentValue += speed * Time.deltaTime;
+    } else {
+    }
+
+    bar.fillAmount = currentValue / 100;
+
+    Debug.Log(currentValue);
+  }
+}

38ea04fc714f51cf9e8e3f24ca19b3d200ee0972
Henry <henry@henrywu.co.uk>
2021-04-23 19:00:18 +0100

    clock

---
+++ Assets/DigitalClock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class DigitalClock : MonoBehaviour {
+  private TextMeshProUGUI t;
+  void Awake() {
+    t = GetComponent<TextMeshProUGUI>();
+  }
+
+  void Update() {
+    DateTime time = DateTime.Now;
+    string hour = LeadingZero(time.Hour);
+    string minute = LeadingZero(time.Minute);
+    string second = LeadingZero(time.Second);
+    t.text = hour + ":" + minute + ":" + second;
+  }
+  string LeadingZero(int n) {
+    return n.ToString().PadLeft(2, '0');
+  }
+}
